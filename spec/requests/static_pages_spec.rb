92d44b09c9e0e94725fe626495a5ba2f9b79fb93
Edward Chan <manmanhokarman@gmail.com>
2013-04-09 19:39:09 +0800

    Updated rspec

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,40 +1,55 @@
 require 'spec_helper'
 
 describe "Static pages" do
+
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
   describe "Home page" do
-    it "Should have the content 'Sample app'" do
+    it "Should have the h1 'Sample app'" do
       visit '/static_pages/home'
-      page.should have_content('Sample App')
+      page.should have_selector('h1', text: 'Sample App')
     end
 
     it "Should have the right title" do
       visit '/static_pages/home'
-      page.should have_selector('title', text: "Ruby on Rails Tutorial Sample App | Home")
+      page.should have_selector('title', text: "#{base_title} | Home")
     end
   end
 
   describe "Help page" do
-    it "Should have the content 'Help'" do
+    it "Should have the h1 'Help'" do
       visit '/static_pages/help'
-      page.should have_content('Help')
+      page.should have_selector('h1', text: 'Help')
     end
 
     it "Should have the right title" do
       visit '/static_pages/help'
-      page.should have_selector('title', text: "Ruby on Rails Tutorial Sample App | Help")
+      page.should have_selector('title', text: "#{base_title} | Help")
     end
   end
 
   describe "About us page" do
-    it "Should have the content 'About us'" do
+    it "Should have the h1 'About us'" do
       visit '/static_pages/about'
-      page.should have_content('About us')
+      page.should have_selector('h1', text: 'About us')
     end
 
     it "should have the right title" do
       visit '/static_pages/about'
-      page.should have_selector('title', text: "Ruby on Rails Tutorial Sample App | About Us")
+      page.should have_selector('title', text: "#{base_title} | About us")
     end
   end
-  
+
+  describe "Contact page" do 
+    it "should have the h1 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('h1', text: 'Contact')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/contact'
+      page.should have_selector('title', text: "#{base_title} | Contact")
+    end
+  end
+
 end
